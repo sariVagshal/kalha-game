8c55e77ea4b96e3cec17ba19c43a77a66caec182
sariVagshal <sari6910@gmail.com>
2018-11-15 16:27:17 +0200

    all what I already did...

--- kalah.py
+++ kalah.py
@@ -1,2 +1,92 @@
-class Kalah(object):
-    pass
+class Kalha:
+    def __init__(self, holes, seeds):
+        self.player = 0
+        self.bank = [0, 0]
+        self.holes = holes
+        self.now = {0: {}, 1: {}}
+        for j in range(2):
+            for i in range(holes):
+                self.now[j][i] = seeds
+        self.winnerr = False
+
+    def __repr__(self):
+        show = ' '
+        for i in range(self.holes):
+            show += str(self.now[0][self.holes-1-i])
+        show += '\n'
+        show += str(self.bank[0]) + " " * self.holes + str(self.bank[1]) + '\n' + ' '
+        for i in range(self.holes):
+            show += str(self.now[1][self.holes-1-i])
+        return show
+
+    def __str__(self):
+        show = 'Player1: '
+        for i in range(self.holes):
+            show += str(self.now[0][self.holes-1-i])
+        show += '\n        '
+        show += str(self.bank[0]) + " " * self.holes + str(self.bank[1]) + '\n' + 'Player2: '
+        for i in range(self.holes):
+            show += str(self.now[1][self.holes-1-i])
+        return show
+
+
+    def play(self, hole):
+        if self.winnerr:
+            return "Game Over"
+        if hole > self.holes:
+            raise IndexError
+        # hole = hole - 1
+        if self.now[self.player][hole - 1] == 0:
+            raise ValueError
+        stones = self.now[self.player][hole - 1]
+        self.now[self.player][hole - 1] = 0
+        play_er = self.player
+        flag = 0
+        while stones:
+            if hole < self.holes:
+                self.now[play_er][hole] += 1
+                hole += 1
+            else:
+                if play_er == self.player:
+                    self.bank[play_er] += 1
+                else:
+                    stones += 1
+                hole = 0
+                if stones - 1 > 0:
+                    play_er = 1 - play_er
+                flag = 1
+            stones = stones - 1
+        # hole = hole-1 if hole-1>=0 else self.holes-1
+        if play_er == self.player:
+            if hole != 0 and self.now[play_er][hole - 1] == 1:
+                self.bank[play_er] += 1
+                self.now[play_er][hole - 1] = 0
+                self.bank[play_er] += self.now[1 - play_er][(self.holes - hole)]
+                self.now[1 - play_er][(self.holes - hole)] = 0
+        if hole != 0 or play_er != self.player:
+            self.player = 1 - self.player
+        if not any(self.now[self.player]):
+            for i in range(self.holes):
+                self.bank[self.player] += self.now[1 - self.player][i]
+        if self.bank[self.player] >= 37:
+            self.winnerr = True
+            return "Player {} wins".format(self.player + 1)
+        if self.bank[1 - self.player] >= 37:
+            self.winnerr = True
+            return "Player {} wins".format(self.player)
+        return "Player 2 plays next" if self.player else "Player 1 plays next"
+
+
+    def status(self):
+        listt = []
+        for j in range(2):
+            for i in range(self.holes):
+                listt.append(self.now[j][i])
+            listt.append([self.bank[j]])
+        return listt
+
+
+    def winner(self):
+        return self.winnerr
+
+
--- test_kalah.py
+++ test_kalah.py
@@ -1,8 +1,170 @@
 import unittest
+#from kalha_tests.kalha import Kalha
+from kalha import Kalha
+#from ..kalha_tests import kalha as Kalha
+#import .kalha as Kalha
+
+class KalhaTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_simple(self):
+        game = Kalha(6, 4)
+        self.assertEqual(game.status(), [4,4,4,4,4,4,[0],4,4,4,4,4,4,[0],])
+        self.assertEqual(game.play(1), "Player 2 plays next")
+        self.assertEqual(game.status(), [0,5,5,5,5,4,[0],4,4,4,4,4,4,[0],])
+        self.assertEqual(game.play(4), "Player 1 plays next")
+        self.assertEqual(game.status(), [1,5,5,5,5,4,[0],4,4,4,0,5,5,[1],])
+        self.assertEqual(game.play(1), "Player 2 plays next")
+        self.assertEqual(game.status(), [0,6,5,5,5,4,[0],4,4,4,0,5,5,[1],])
+        self.assertRaises(IndexError,game.play,7)
+        self.assertRaises(ValueError,game.play,4)
+
+    def test_game1(self):
+        game = Kalha(6, 6)
+        self.assertEqual(game.status(), [6, 6, 6, 6, 6, 6, [0], 6, 6, 6, 6, 6, 6, [0], ])
+        self.assertEqual(game.play(2), "Player 2 plays next")
+        self.assertEqual(game.status(), [6, 0, 7, 7, 7, 7, [1], 7, 6, 6, 6, 6, 6, [0], ])
+        self.assertEqual(game.play(1), "Player 1 plays next")
+        self.assertEqual(game.play(4), "Player 2 plays next")
+        self.assertEqual(game.status(), [7, 0, 7, 0, 8, 8, [2], 1, 8, 8, 8, 7, 7, [1],])
+        self.assertEqual(game.play(2), "Player 1 plays next")
+        self.assertEqual(game.play(1), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 2, 9, 1, 9, 9, [3], 2, 1, 9, 9, 8, 8, [2], ])
+        self.assertEqual(game.play(2), "Player 1 plays next")
+        self.assertEqual(game.play(5), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 2, 9, 1, 0, 10, [14], 3, 1, 11, 10, 9, 0, [2], ])
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 2 plays next")
+        self.assertEqual(game.status(), [2, 4, 11, 3, 1, 0, [15], 5, 2, 12, 11, 1, 2, [3], ])
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(4), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(2), "Player 2 plays next")
+        self.assertEqual(game.status(), [2, 0, 12, 1, 1, 0, [25], 0, 2, 12, 11, 0, 3, [3], ])
+        self.assertEqual(game.play(2), "Player 1 plays next")
+        self.assertEqual(game.play(3), "Player 2 plays next")
+        self.assertEqual(game.status(), [3, 0, 0, 2, 2, 1, [28], 1, 1, 14, 13, 0, 4, [3], ])
+        self.assertEqual(game.play(1), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(4), "Player 2 plays next")
+        self.assertEqual(game.status(), [3, 0, 0, 0, 1, 0, [32], 0, 2, 14, 13, 0, 4, [3], ])
+        self.assertEqual(game.play(2), "Player 1 plays next")
+        self.assertEqual(game.play(1), "Player 1 wins")
+        self.assertEqual(game.status(), [0, 1, 1, 0, 1, 0, [48], 0, 0, 0, 14, 0, 4, [3], ])
+        self.assertEqual(game.play(1), "Game Over")
+
+    def test_game2(self):
+        game = Kalha(6, 6)
+        self.assertEqual(game.play(2), "Player 2 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(1), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 2, 9, 9, 9, 8, [2], 8, 6, 6, 6, 6, 0, [1], ])
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(5), "Player 2 plays next")
+        self.assertEqual(game.status(), [2, 3, 10, 10, 0, 9, [3], 9, 7, 7, 7, 1, 2, [2], ])
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(2), "Player 2 plays next")
+        self.assertEqual(game.status(), [3, 0, 11, 11, 0, 9, [11], 9, 0, 7, 7, 1, 0, [3], ])
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(3), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 0, 0, 12, 1, 10, [14], 10, 1, 8, 8, 1, 0, [7], ])
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 2 plays next")
+        self.assertEqual(game.status(), [1, 1, 0, 12, 1, 0, [25], 11, 2, 9, 0, 1, 1, [8], ])
+        self.assertEqual(game.play(1), "Player 1 plays next")
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(4), "Player 1 wins")
+        self.assertEqual(game.status(), [3, 3, 2, 0, 1, 1, [40], 1, 4, 0, 2, 3, 3, [9], ])
+
+    def test_game3(self):
+        game = Kalha(6, 6)
+        self.assertEqual(game.play(2), "Player 2 plays next")
+        self.assertEqual(game.play(1), "Player 1 plays next")
+        self.assertEqual(game.play(4), "Player 2 plays next")
+        self.assertEqual(game.status(), [7, 0, 7, 0, 8, 8, [2], 1, 8, 8, 8, 7, 7, [1], ])
+        self.assertEqual(game.play(1), "Player 1 plays next")
+        self.assertEqual(game.play(1), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 1, 8, 1, 9, 9, [3], 1, 9, 8, 8, 7, 7, [1], ])
+        self.assertEqual(game.play(2), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 2 plays next")
+        self.assertEqual(game.status(), [2, 3, 9, 2, 9, 0, [4], 2, 1, 10, 10, 9, 9, [2], ])
+        self.assertEqual(game.play(1), "Player 1 plays next")
+        self.assertEqual(game.play(1), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 4, 10, 2, 9, 0, [4], 0, 2, 11, 10, 9, 9, [2], ])
+        self.assertEqual(game.play(2), "Player 1 plays next")
+        self.assertEqual(game.play(5), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 4, 10, 2, 0, 1, [16], 1, 1, 13, 12, 10, 0, [2], ])
+        self.assertEqual(game.play(1), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(4), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 4, 10, 0, 1, 0, [18], 0, 2, 13, 12, 10, 0, [2], ])
+        self.assertEqual(game.play(2), "Player 1 plays next")
+        self.assertEqual(game.play(5), "Player 2 plays next")
+        self.assertEqual(game.status(), [0, 4, 10, 0, 0, 0, [19], 0, 0, 14, 13, 10, 0, [2], ])
+        self.assertEqual(game.play(3), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(2), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(5), "Player 1 plays next")
+        self.assertEqual(game.play(6), "Player 1 plays next")
+        self.assertEqual(game.play(3), "Player 1 wins")
+        self.assertEqual(game.status(), [2, 0, 0, 3, 1, 1, [38], 2, 2, 2, 16, 0, 2, [3], ])
+
+    def test_simulate(self):
+        dic_p1 = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6}
+        dic_p2 = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6}
+        path = "simulate.txt"
+        game = Kalha(6,6)
+        with open(path, 'r') as f:
+            content = f.read().splitlines()
+        for line in content:
+            self.assertFalse(game.winnerr)
+            print(line)
+            line = line[line.find('.')+2:]
+            p1, p2 = line.split()
+            #p1
+            prev = game.bank[0]
+            if p1.find('(')!=-1:
+                actions, bank = p1.split('(')
+                bank = bank.split(')')
+                bank = int(bank[0][1:])
+            else:
+                actions = p1
+                bank = 0
+            if actions.find('-') != -1:
+                actions=actions.split('-')
+            else:
+                actions=[actions]
+            for i in actions:
+                game.play(dic_p1[i])
+            self.assertEqual(bank,game.bank[0]-prev)
+            # p2
+            prev = game.bank[1]
+            if p2.find('(')!=-1:
+                actions, bank = p2.split('(')
+                bank = bank.split(')')
+                bank = int(bank[0][1:])
+            else:
+                actions = p2
+                bank = 0
+            if actions.find('-') != -1:
+                actions=actions.split('-')
+            else:
+                actions=[actions]
+            for i in actions:
+                game.play(dic_p2[i])
+            self.assertEqual(bank, game.bank[1] - prev)
+        self.assertTrue(game.winnerr)
 
 
-class KalahTestCase(unittest.TestCase):
-    pass
 
 
 if __name__ == '__main__':
